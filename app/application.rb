81e1042a18fbb5c8471c5a4c1d36d96dd1500327
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-03 14:45:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,9 @@
 class Application
   def call(env)
+    resp = Rack::Response.new
+    if time.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
     
